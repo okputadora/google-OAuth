1e5ff8e09bbbc436455000f9cbba4024c0c84afb
okputadora <okputadora@gmail.com>
2018-05-17 10:40:15 -0400

    authorize user

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const {google} = require('googleapis');
+require('dotenv').config();
 
+const oauth2Client = new google.auth.OAuth2(
+  process.env.CLIENT_ID,
+  process.env.CLIENT_SECRET,
+  process.env.REDIRECT_URI
+);
 
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  const oauth2Client = new google.auth.OAuth2(
-    process.env.CLIENT_ID,
-    process.env.CLIENT_SECRET,
-    process.env.REDIRECT_URI
-  );
-  // generate a url that asks permissions for Google+ and Google Calendar scopes
+  // generate a url that asks permissions for Google Drive and Google spreadsheets scopes
   const scopes = [
     'https://www.googleapis.com/auth/drive',
     'https://www.googleapis.com/auth/spreadsheets'
@@ -21,7 +22,6 @@ router.get('/', function(req, res, next) {
   const url = oauth2Client.generateAuthUrl({
     // 'online' (default) or 'offline' (gets refresh_token)
     access_type: 'offline',
-
     // If you only need one scope you can pass it as a string
     scope: scopes
   });
@@ -31,10 +31,10 @@ router.get('/', function(req, res, next) {
 
 router.get('/confirmation', function(req, res, next) {
   const code = req.query.code
-  oatuh2Client.getToken(code)
+  oauth2Client.getToken(code)
   .then(function(response) {
     const token = response
-    oauth2Client.setCredentials(tokens);
+    oauth2Client.setCredentials(token);
     res.render('confirmation')
   })
 })
